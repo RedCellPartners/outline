da4e605b49a529bbeb9cc02fef9fa2b3cc0befe2
Trever Pietsch <trever.pietsch@redcellpartners.com>
2024-08-22 17:49:18 -0700

    see if it can find crt file

--- server/utils/ssl.ts
+++ server/utils/ssl.ts
@@ -22,15 +22,17 @@ export function getSSLOptions() {
     return {
       key:
         (env.SSL_KEY
-          ? Buffer.from(env.SSL_KEY).toString("ascii")
+          ? Buffer.from(env.SSL_KEY, "base64").toString("ascii")
           : undefined) ||
+        safeReadFile("tls.key") ||
         safeReadFile("private.key") ||
         safeReadFile("private.pem") ||
         safeReadFile("server/config/certs/private.key"),
       cert:
         (env.SSL_CERT
-          ? Buffer.from(env.SSL_CERT).toString("ascii")
+          ? Buffer.from(env.SSL_CERT, "base64").toString("ascii")
           : undefined) ||
+        safeReadFile("tls.crt") ||
         safeReadFile("public.cert") ||
         safeReadFile("public.pem") ||
         safeReadFile("server/config/certs/public.cert"),
