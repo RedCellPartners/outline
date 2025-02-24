5a90cb470f8bd2504b0600ef339596bc3f3c0fd9
Trever Pietsch <trever.pietsch@redcellpartners.com>
2025-02-23 16:03:27 -0800

    kill repo

--- shared/editor/version.ts
+++
@@ -1,3 +0,0 @@
-const EDITOR_VERSION = "13.0.0";
-
-export default EDITOR_VERSION;
--- shared/utils/routeHelpers.ts
+++
@@ -1,7 +0,0 @@
-export function signin(service = "slack"): string {
-  return `/auth/${service}`;
-}
-
-export function integrationSettingsPath(id: string): string {
-  return `/settings/integrations/${id}`;
-}
